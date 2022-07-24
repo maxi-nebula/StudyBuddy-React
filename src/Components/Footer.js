adfc74ca7291145ebbebcddd1213cab0bdf538d6
Shyni Madhumathi Ravichandran <109126852+maxi-nebula@users.noreply.github.com>
2022-07-23 19:52:16 -0700

    from IDE

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="footer">For all you study related activities!</div>;
+  return <div className="footer">For all you study related activities!!</div>;
 };
 
 export default Footer;
